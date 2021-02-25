88cd28c8d1779d855457186b0666743f8429bb54
Maho Sasaki <msasaki.a@gmail.com>
2021-02-25 15:49:48 -0600

    remove unused fonts and file

--- src/Components/MainTableView/InsertTuple/InsertTuple.tsx
+++ src/Components/MainTableView/InsertTuple/InsertTuple.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faRedoAlt, faTrashAlt, faPlusCircle, faExclamationCircle} from '@fortawesome/free-solid-svg-icons'
+import {faTrashAlt, faPlusCircle, faExclamationCircle} from '@fortawesome/free-solid-svg-icons'
 import TableAttribute from '../DataStorageClasses/TableAttribute';
 import TableAttributesInfo from '../DataStorageClasses/TableAttributesInfo';
 import PrimaryTableAttribute from '../DataStorageClasses/PrimaryTableAttribute';
