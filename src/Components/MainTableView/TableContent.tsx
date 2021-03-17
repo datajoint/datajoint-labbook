6914b78b7a45abcf8f5fd409ddb457ba354a918e
Maho Sasaki <msasaki.a@gmail.com>
2021-03-17 12:01:32 -0500

    match format for import bracket

--- src/Components/MainTableView/TableContent.tsx
+++ src/Components/MainTableView/TableContent.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject } from 'react';
+import React, {RefObject} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faChevronRight, faChevronLeft, faStepBackward, faStepForward, faFilter, faPlusCircle, faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons'
 import './TableContent.css'
