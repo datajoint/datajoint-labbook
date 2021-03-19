5a71883631c754f6ecca9e069e6851eb8cfcf7c3
Zhiwei Liang <zhi.wei.liang@outlook.com>
2021-03-18 22:23:55 -0700

    Update login page test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,15 +4,31 @@ import App from './Components/App';
 
 test('renders login page', () => {
     render(<App />);
+
+    // All inner text
     const linkElements = [
         screen.getByText("Host\/Database Address"),
         screen.getByText("Username"),
         screen.getByText("Password"),
         screen.getByText("Remember Me"),
-        screen.getByText("Connect")
+        screen.getByText("Connect"),
+        screen.getByText("Log In")
     ];
+
+    // Navigation bar
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+
+    // `Remember Me` checkbox
+    expect(screen.getByRole('checkbox')).toBeInTheDocument();
+
+    // Logos
+    expect(screen.getByAltText('Logo')).toBeInTheDocument();
+    expect(screen.getByAltText('datajoint gui logo')).toBeInTheDocument();
+
+    // `Connect` Button
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeDisabled();
     linkElements.forEach(element => {
         expect(element).toBeInTheDocument();
     })
-
 });
