5dfe0c574659ec279cadc5a4190fb1b0af7580db
Zhiwei Liang <zhi.wei.liang@outlook.com>
2021-03-26 22:20:58 -0700

    Add Login operation mocking test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, cleanup } from '@testing-library/react';
 import App from './Components/App';
+import Login from './Components/Login/Login';
+import Home from './Components/Home'
 
-test('renders login page', () => {
+
+test('Verifying components appear on login page.', async () => {
     render(<App />);
 
     // All inner text
@@ -32,3 +35,24 @@ test('renders login page', () => {
         expect(element).toBeInTheDocument();
     })
 });
+
+test('Login operation mocking', async () => {
+    const result = render(
+        <App />
+    );
+    //console.log(screen.getByLabelText(""));
+    const passwordInput = result.container.querySelector('#password');
+    const usernameInput = result.container.querySelector('#username');
+    const dbServerInput = result.container.querySelector('#database-server');
+    if (passwordInput && usernameInput && dbServerInput) {
+        fireEvent.change(usernameInput, {target : {value : "root"}})
+        fireEvent.change(passwordInput, {target : {value : "labbook"}})
+        fireEvent.change(dbServerInput, {target : {value : "local-db"}})
+        const connectButton = screen.getByText('Connect');
+        expect(connectButton).toBeEnabled();
+        fireEvent.click(connectButton);        
+    } else {
+        throw new Error('textbox for login info not found.');
+    }
+});
+
